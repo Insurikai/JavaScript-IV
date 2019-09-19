193d5e17a172686c45fb65b935f81652bee8c126
Insurikai <40343703+Insurikai@users.noreply.github.com>
2019-09-19 12:54:45 -0700

    ProjectManager standUp finished

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -44,6 +44,6 @@ class ProjectManager extends Instructor{
         this.gradClassName = args.gradClassName;
         this.favInstructor = args.favInstructor;
     }
-    standUp(){}
+    standUp(channel){ return `${this.name} announces to ${channel}, @channel standy times!​​​​​`;}
     debugsCode(){}
 }
